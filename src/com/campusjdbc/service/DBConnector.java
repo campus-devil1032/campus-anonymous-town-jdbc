65d410f52278e8bd0e3c59cb4655d61a89256884
ej31 <devil1032@gmail.com>
2023-05-19 03:44:29 +0900

    remove sensitive info

--- src/com/campusjdbc/service/DBConnector.java
+++ src/com/campusjdbc/service/DBConnector.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-    private static final String dbUrl = "jdbc:mysql://13.209.76.58:3306/somebody_town"; // your database url
-    private static final String username = "jeff"; // your database username
-    private static final String password = "dlaxowhd"; // your database password
+    private static final String dbUrl = "jdbc:mysql://ip입력:3306/스키마입력"; // your database url
+    private static final String username = "mysqluser"; // your database username
+    private static final String password = "mysql비밀번호"; // your database password
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(dbUrl, username, password);
